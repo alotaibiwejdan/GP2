dfd173cccaf82dcaa265d4706cd0b28ceceabed9
Khulud09 <162730946+Khulud09@users.noreply.github.com>
2026-03-07 04:41:54 +0300

    Change home screen to OnboardingScreen
    Updated the main application entry point to use OnboardingScreen as the initial page instead of LoginScreen.

--- main.dart
+++ main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'settings_page.dart';
-import 'group_meeting_page.dart';
-import 'tracking_page.dart';
-// 1. أضيفي استيراد ملف صديقتك هنا (تأكدي من اسم الملف إذا كان login_screen.dart)
-import 'login_screen.dart'; 
+// import 'settings_page.dart';
+// import 'group_meeting_page.dart';
+// import 'tracking_page.dart';
+// import 'login_screen.dart'; We used it when the login was the first page
+import 'onboarding_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -21,8 +22,8 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Tajawal', 
         useMaterial3: true,
       ),
-      // 2. هنا نغير الصفحة الأساسية لتكون شاشة تسجيل الدخول
-      home: const LoginScreen(), 
+      // 2.  هنا نغير الصفحة الأساسية و الأولى
+      home: const OnboardingScreen(), 
     );
   }
 }
