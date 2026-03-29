de6f7c5173bbc5900174f196b80c94386eaf8821
alotaibiwejdan <161254236+alotaibiwejdan@users.noreply.github.com>
2026-03-29 22:50:47 +0300

    Refactor MyApp to StatefulWidget and add theme support
    Converted MyApp to a StatefulWidget to allow theme changes. Added multiple imports for new pages and implemented theme switching functionality.

--- main.dart
+++ main.dart
@@ -1,28 +1,57 @@
 import 'package:flutter/material.dart';
-// import 'settings_page.dart';
-// import 'group_meeting_page.dart';
-// import 'tracking_page.dart';
-// import 'login_screen.dart'; We used it when the login was the first page
 import 'onboarding_screen.dart';
-
+// 1. لازم تستوردين صفحة الإعدادات عشان يقرأ المتغير العام
+import 'settings_page.dart'; 
+import 'appointment_details_page.dart';
+import 'appointment_page.dart';
+import 'edit_appointment_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+// 2. حولنا MyApp إلى StatefulWidget عشان يقدر "يعيد بناء نفسه" لما يتغير الثيم
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  // هذه السطر بنحتاجه عشان ننادي إعادة البناء من أي مكان
+  static _MyAppState? of(BuildContext context) => context.findAncestorStateOfType<_MyAppState>();
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  
+  // دالة لتحديث الواجهة كاملة
+  void changeTheme() {
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, 
       title: 'مرسال',
+      
+      // 3. هنا الربط السحري بالمتغير اللي في صفحة الإعدادات
+      themeMode: isGlobalDarkMode ? ThemeMode.dark : ThemeMode.light,
+      
       theme: ThemeData(
         fontFamily: 'Tajawal', 
         useMaterial3: true,
+        brightness: Brightness.light,
+        primarySwatch: Colors.purple,
       ),
-      // 2.  هنا نغير الصفحة الأساسية و الأولى
+      
+      // إعدادات الثيم الداكن (الأسود)
+      darkTheme: ThemeData(
+        fontFamily: 'Tajawal',
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color(0xFF121212),
+      ),
+      
       home: const OnboardingScreen(), 
     );
   }
